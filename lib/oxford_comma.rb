fc90d856d81dccb6c972e8c8e66ca04983766b35
Leticia Molina <letimolina@gmail.com>
2018-05-20 21:37:37 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length > 2
    arr2 = array.shift(array.length-1)
    str1 = arr2.join(", ")
-   str2 = ", and #{array[array.length-1]}."
+   str2 = ", and #{array[array.length-1]}"
    str1 << str2
   elsif array.length == 2
     array.join(" and ")
