feede98b6df7552162274a6d58d8110a99e0f4e9
Leticia Molina <letimolina@gmail.com>
2018-05-20 21:37:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length > 2
+   arr2 = array.shift(array.length-1)
+   str1 = arr2.join(", ")
+   str2 = ", and #{array[array.length-1]}"
+   str1 << str2
+  elsif array.length == 2
+    array.join(" and ")
+  else 
+    array * " "
+end
 end
