348526248b98103c2823ade970a39f63b86f3fa2
Leticia Molina <letimolina@gmail.com>
2018-05-20 21:30:30 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,9 @@
 def oxford_comma(array)
+  if array.length > 1
    arr2 = array.shift(array.length-1)
    str1 = arr2.join(", ")
    str2 = ", and #{array[array.length-1]}."
    str1 << str2
+  else
+    array
 end
